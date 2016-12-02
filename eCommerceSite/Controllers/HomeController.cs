aea7f0b2eeed744d439c73dce88c5d83e083113d
britzkopf <britzkopf@users.noreply.github.com>
2016-12-02 14:42:40 -0800

    removed field underscore prefix

--- eCommerceSite/Controllers/HomeController.cs
+++ eCommerceSite/Controllers/HomeController.cs
@@ -11,19 +11,19 @@ namespace eCommerceSite.Controllers
 {
     public class HomeController : Controller
     {
-        private IMailService _mail;
-        private eCommerceRepository _rep;
+        private IMailService mail;
+        private eCommerceRepository rep;
         public HomeController(IMailService mail)
         {
             
-            _mail = mail;
-            _rep = new eCommerceRepository();
+            mail = mail;
+            rep = new eCommerceRepository();
         }
 
         public ActionResult Index()
         {
 
-            var items = _rep.GetItems();
+            var items = rep.GetItems();
             return View(items);
         }
 
@@ -47,7 +47,7 @@ namespace eCommerceSite.Controllers
             var msg = string.Format("Comment from: {1}{0}Email:{2}{0}subject:{3}{0} Comment:{4}{0}",
                 Environment.NewLine, model.name, model.email, model.subject, model.message);
 
-            if (_mail.SendMail("companyemail@company.com", "customer@customer.com", "website contact", msg))
+            if (mail.SendMail("companyemail@company.com", "customer@customer.com", "website contact", msg))
             {
                 ViewBag.MailSent = true;
             }
@@ -60,17 +60,17 @@ namespace eCommerceSite.Controllers
         }
         public ActionResult Item(int Id) 
         {
-            var x = _rep.GetItemDetails().Where(i => i.ItemId == Id).ToList();
-            ViewData["Reviews"] = _rep.GetReviewsByItem(Id).ToList();
+            var x = rep.GetItemDetails().Where(i => i.ItemId == Id).ToList();
+            ViewData["Reviews"] = rep.GetReviewsByItem(Id).ToList();
             ViewData["ItemDetails"] = x;
-            ViewData["ReviewsCount"] = _rep.GetReviewsByItem(Id).ToList().Count;
-            var result = _rep.GetObjects().Find(Id);
+            ViewData["ReviewsCount"] = rep.GetReviewsByItem(Id).ToList().Count;
+            var result = rep.GetObjects().Find(Id);
             
             return View(result);
         }
         public FileContentResult ImageRender(int Id)
         {
-            byte[] ImgArray = _rep.GetObjects().Find(Id).Thumbnail;
+            byte[] ImgArray = rep.GetObjects().Find(Id).Thumbnail;
             return new FileContentResult(ImgArray, "image/jpeg");
         }
     }
